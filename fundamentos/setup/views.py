62d9fe088bb580e25389c242c1d7b4a2d978e835
Adriano B Cerutti <adrianocerutti@gmail.com>
2025-08-08 15:19:29 -0300

    Commit para mudanca de maquina

---
+++ fundamentos/setup/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def homepage(request):
+    nome = "Adriano"
+    pessoa = {
+        'nome': 'Maria',
+        'idade': 30,
+        'cidade': 'São Paulo'
+    }
+    return render(request, 'home.html', {'nome': nome, 'pessoa': pessoa})
+
+
+def about(request):
+    frutas = ['Maçã', 'Banana', 'Laranja', 'Uva']
+    return render(request, 'about.html', {'frutas': frutas})
