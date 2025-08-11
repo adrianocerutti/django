f401940679b8d31b02e13571673ae0447e6a01af
Adriano B Cerutti <adrianocerutti@gmail.com>
2025-08-11 17:21:39 -0300

    Usando URL Nomeada

--- fundamentos/products/urls.py
+++ fundamentos/products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.products_list),
+    path('', views.products_list, name="products"),
 ]
--- fundamentos/products/views.py
+++ fundamentos/products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Product
 
 # Create your views here.
@@ -7,3 +8,7 @@ from .models import Product
 def products_list(request):
     products = Product.objects.all().order_by('created_at')
     return render(request, 'products/products_list.html', {'products': products})
+
+
+def product_page(request, slug):
+    return HttpResponse(slug)
