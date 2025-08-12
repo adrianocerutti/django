836841f0965bdfae0e8ad50b731540a9888d091b
Adriano B Cerutti <adrianocerutti@gmail.com>
2025-08-12 12:29:42 -0300

    Criadas as pastas para Upload de Imagem

--- fundamentos/products/models.py
+++ fundamentos/products/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
     slug = models.SlugField()
+    banner = models.ImageField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
